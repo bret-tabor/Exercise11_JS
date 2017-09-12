1e943f45be3e1eae2177fd3c2906d0c93e9d3cb8
Bret Tabor <bret.tabor@gmail.com>
2017-09-11 21:05:30 -0500

    updated js and css files

--- Exercises/Exercise14/scripts/main.js
+++ Exercises/Exercise14/scripts/main.js
@@ -7,33 +7,33 @@ function submitForm() {
     var Name = getValue('Name');
     if (Name.length === 0) {
         messages.push(' Your name is required');
-        getElm('Name').style.background = "#FF8F87";
+        getElm('Name').classList.add("errorBox");
     } else {
-        getElm('Name').style.background = "#FFF";
+        getElm('Name').classList.remove("errorBox");
     }
     //If Email is empty. Add it to the array
     var Email = getValue('Email');
     if (Email.includes('@') === false || Email.includes('.com') === false) {
         messages.push(' A valid email address is required');
-        getElm('Email').style.background = "#FF8F87";
+        getElm('Email').classList.add("errorBox");
     } else {
-        getElm('Email').style.background = "#FFF";
+        getElm('Email').classList.remove("errorBox");
     }
     // If Password is empty. Add it to the array
     var Password = getValue('Password');
     if (Password.length === 0) {
         messages.push(' A password is required');
-        getElm('Password').style.background = "#FF8F87";
+        getElm('Password').classList.add("errorBox");
     } else {
-        getElm('Password').style.background = "#FFF";
+        getElm('Password').classList.remove("errorBox");
     }
     // If Time Zone is not selected. Add it to the array
     var TimeZoneSelect = getValue('TimeZoneSelect');
     if (TimeZoneSelect.length === 0) {
         messages.push(' You must select a Time Zone');
-        getElm('TimeZoneSelect').style.background = "#FF8F87";
+        getElm('TimeZoneSelect').classList.add("errorBox");
     } else {
-        getElm('TimeZoneSelect').style.background = "#FFF";
+        getElm('TimeZoneSelect').classList.remove("errorBox");
     }
     //Loop through array and add messages to the innerHTML of the error-list id
     for (i = 0; i < messages.length; i++) {
@@ -50,17 +50,18 @@ function submitForm() {
 
 }
 
-
-
-
-// clicking submit creates an alert dialog
+// clicking submit will create a succesful alert dialog or display error messages
 getElm('Submit').addEventListener('click', submitForm);
 
 // clicking reset empties inputs
 getElm('Reset').addEventListener('click', function () {
 
     document.forms["myForm"].reset();
-
+    getElm('Name').classList.remove("errorBox");
+    getElm('Email').classList.remove("errorBox");
+    getElm('Password').classList.remove("errorBox");
+    getElm('TimeZoneSelect').classList.remove("errorBox");
+    getElm("error-list").innerHTML = "";
 });
 
 // getElementById
