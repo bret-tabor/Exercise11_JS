c1463e55e9f3ccb39c7f6b372495da8f65eb3e42
bret-tabor <tarrickbret@gmail.com>
2017-09-21 20:23:00 -0500

    optimized js

--- Exercises/Exercise17/scripts/main.js
+++ Exercises/Exercise17/scripts/main.js
@@ -1,44 +1,39 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-//hides all elements with the class "error"
+    //hides all elements with the class "error"
     $('.error').hide();
 
-//select even elements with the class "input-group" and use .filter() to get even elements
+    //select even elements with the class "input-group" and use .filter() to get even elements
     $('.input-group').filter(':even').addClass('even');
 
-//add blur to id 'firstName' and show error message if empty
-$('#firstName').on('blur', function() {
-    var firstName = $('#firstName').val();
-    var errMsg = $(this).closest('.input-group').find('.requiredMsg');
-    if (firstName == "") {
-        errMsg.fadeIn();
-    }
-    else {
-        errMsg.fadeOut();
-    }
-})
-//add blur to id 'lastName' and show error message if empty
-$('#lastName').on('blur', function() {
-    var lastName = $('#lastName').val();
-    var errMsg = $(this).closest('.input-group').find('.requiredMsg');
-    if (lastName == "") {
-        errMsg.fadeIn();
-    }
-    else {
-        errMsg.fadeOut();
-    }
-})
-//add blur to id 'emailAddress' and show error message if: empty, doesn't contain '@', or doesn't contain '.com'
-$('#emailAddress').on('blur', function() {
-    var emailAddress = $('#emailAddress').val();
-    var errMsg = $(this).closest('.input-group').find('.emailMsg');
-    if (emailAddress == "" || emailAddress.includes('@') === false || emailAddress.includes('.com') === false ) {
-        errMsg.fadeIn();
-    }
-    else {
-        errMsg.fadeOut();
-    }
-})
+    //reusable error message function
+    function validate() {
+        var input = $(this);
+        var inputValue = input.val();
+        var errMsg = input.closest('.input-group').find('.requiredMsg');
 
+        if (inputValue === '') {
+            errMsg.fadeIn();
+        } else {
+            errMsg.fadeOut();
+        }
+    }
+    //error message for invalid email
+    function validateEmail() {
+        var input = $(this);
+        var inputValue = input.val();
+        var errMsg = input.closest('.input-group').find('.emailMsg');
+
+        if (inputValue.indexOf('@') === -1 || inputValue.indexOf('.com') === -1) {
+            errMsg.fadeIn();
+        } else {
+            errMsg.fadeOut();
+        }
+    }
+    //All hail jQuery
+    $('#lastName').on('blur', validate);
+    $('#firstName').on('blur', validate);
+    $('#emailAddress').on('blur', validate);
+    $('#emailAddress').on('blur', validateEmail);
 
 });
